ad154998ea006a21588407e0aefe843e7815d2ee
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2024-11-17 18:45:59 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    tyys: process.env.tyys || '',
+    WX_PUSHER_APP_TOKEN: process.env.WX_PUSHER_APP_TOKEN || "AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr",
+    WX_PUSHER_UID: process.env.WX_PUSHER_UID || "",
+};
